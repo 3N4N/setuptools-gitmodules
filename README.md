fda9d48a9268baf30681f2ab5746c2d52233a263
Enan Ajmain <3nan.ajmain@gmail.com>
2023-07-10 21:05:32 +0600

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# setuptools-gitmodules
+
+Dependency management via git submodules.
+
+Work in progress.
---
+++ src/setuptools_gitmodules/__init__.py
@@ -0,0 +1,9 @@
+import toml
+from collections.abc import Mapping
+from pathlib import Path
+
+
+def finalize_dist(dist):
+    print(" ===================== finalize_dist")
+
+
