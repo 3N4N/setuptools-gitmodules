accccd2b48b6fa2146c32477956bfe66f93a0d55
Enan Ajmain <3nan.ajmain@gmail.com>
2023-07-11 00:50:42 +0600

    feat(wip): manipulate install dependencies of dist

--- src/setuptools_gitmodules/__init__.py
+++ src/setuptools_gitmodules/__init__.py
@@ -2,8 +2,21 @@ import toml
 from collections.abc import Mapping
 from pathlib import Path
 
+def install_requires_setter(values):
+    pass
+
+def get_git_submodules():
+    # TODO: get git submodule info
+    return [ 'hardcoded-dep1' ]
 
 def finalize_dist(dist):
-    print(" ===================== finalize_dist")
+    pp = toml.load('pyproject.toml')
+    deps = pp.get('project')['dependencies']
+    deps += get_git_submodules()
+    print(f"Install dependencies: {deps}")
+    setattr(dist,'install_requires', deps)
 
-
+    # 'deps' replaced with that from pyproject.toml by setuptools
+    # Unless we bind this function to a nothing function
+    # I agree, it's a terrible way.  Not sure reliable to be stable.
+    setattr(dist.metadata,'set_install_requires', install_requires_setter)
