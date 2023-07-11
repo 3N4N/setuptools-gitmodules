cbfc1066d4c57a26d6030f3ed27c1f5d78507379
Enan Ajmain <3nan.ajmain@gmail.com>
2023-07-11 09:41:35 +0600

    fix: bind set_install_requires attribute to dist

--- src/setuptools_gitmodules/__init__.py
+++ src/setuptools_gitmodules/__init__.py
@@ -1,22 +1,22 @@
 import toml
 from collections.abc import Mapping
 from pathlib import Path
-
-def install_requires_setter(values):
-    pass
+from types import MethodType
 
 def get_git_submodules():
     # TODO: get git submodule info
     return [ 'hardcoded-dep1' ]
 
-def finalize_dist(dist):
+def install_requires_setter(self, values):
     pp = toml.load('pyproject.toml')
     deps = pp.get('project')['dependencies']
     deps += get_git_submodules()
     print(f"Install dependencies: {deps}")
-    setattr(dist,'install_requires', deps)
+    setattr(self,'install_requires', deps)
 
+def finalize_dist(dist):
     # 'deps' replaced with that from pyproject.toml by setuptools
-    # Unless we bind this function to a nothing function
-    # I agree, it's a terrible way.  Not sure reliable to be stable.
-    setattr(dist.metadata,'set_install_requires', install_requires_setter)
+    # Unless we bind this attribute 'set_install_requires' to a function
+    # And the function needs to be bound to dist object, otherwise it can't
+    # update the dist object.  I don't know if this is the best way.  But it works.
+    setattr(dist.metadata,'set_install_requires', MethodType(install_requires_setter, dist))
